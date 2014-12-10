740fd34631f64a782c335bc5147424605362a8e5
Shinku443 <steven85@purdue.edu>
2014-12-09 19:39:01 -0500

    Updated pages

--- app/build/generated/source/r/debug/info/androidhive/slidingmenu/R.java
+++ app/build/generated/source/r/debug/info/androidhive/slidingmenu/R.java
@@ -35,29 +35,34 @@ public final class R {
     }
     public static final class drawable {
         public static final int counter_bg=0x7f020000;
-        public static final int ic_communities=0x7f020001;
-        public static final int ic_drawer=0x7f020002;
-        public static final int ic_home=0x7f020003;
-        public static final int ic_launcher=0x7f020004;
-        public static final int ic_pages=0x7f020005;
-        public static final int ic_people=0x7f020006;
-        public static final int ic_photos=0x7f020007;
-        public static final int ic_whats_hot=0x7f020008;
-        public static final int list_item_bg_normal=0x7f020009;
-        public static final int list_item_bg_pressed=0x7f02000a;
-        public static final int list_selector=0x7f02000b;
+        public static final int graph=0x7f020001;
+        public static final int ic_communities=0x7f020002;
+        public static final int ic_drawer=0x7f020003;
+        public static final int ic_home=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int ic_pages=0x7f020006;
+        public static final int ic_people=0x7f020007;
+        public static final int ic_photos=0x7f020008;
+        public static final int ic_whats_hot=0x7f020009;
+        public static final int list_item_bg_normal=0x7f02000a;
+        public static final int list_item_bg_pressed=0x7f02000b;
+        public static final int list_selector=0x7f02000c;
     }
     public static final class id {
-        public static final int action_settings=0x7f0b000b;
+        public static final int action_settings=0x7f0b000f;
         public static final int button=0x7f0b0009;
+        public static final int chooseMetric=0x7f0b000d;
         public static final int counter=0x7f0b0005;
+        public static final int delete_graph=0x7f0b000b;
         public static final int drawer_layout=0x7f0b0000;
         public static final int frame_container=0x7f0b0001;
+        public static final int graph_button=0x7f0b000c;
         public static final int icon=0x7f0b0003;
+        public static final int imageView=0x7f0b000a;
         public static final int listView=0x7f0b0008;
         public static final int list_slidermenu=0x7f0b0002;
         public static final int metricButton=0x7f0b0007;
-        public static final int testplot=0x7f0b000a;
+        public static final int testplot=0x7f0b000e;
         public static final int title=0x7f0b0004;
         public static final int txtLabel=0x7f0b0006;
     }
@@ -70,7 +75,8 @@ public final class R {
         public static final int fragment_pages=0x7f030005;
         public static final int fragment_photos=0x7f030006;
         public static final int fragment_whats_hot=0x7f030007;
-        public static final int testplot=0x7f030008;
+        public static final int graphing=0x7f030008;
+        public static final int testplot=0x7f030009;
     }
     public static final class menu {
         public static final int main=0x7f0a0000;
--- app/src/main/java/info/androidhive/slidingmenu/PlotFragment.java
+++ app/src/main/java/info/androidhive/slidingmenu/PlotFragment.java
@@ -16,8 +16,8 @@ public class PlotFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View rootView = inflater.inflate(R.layout.testplot, container, false);
-
+        //View rootView = inflater.inflate(R.layout.testplot, container, false);
+        View rootView = inflater.inflate(R.layout.graphing, container, false);
 
         return rootView;
     }
